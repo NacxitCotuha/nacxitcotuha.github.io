40d65d302653ff99aca0c43197a3c76226ef72ec
NacxitTrejo <nacxit.cotuha@gmail.com>
2026-04-21 23:09:05 -0600

    FEAT:Se agrega Animacion de escritura x2

--- src/assets/js/render.js
+++ src/assets/js/render.js
@@ -26,22 +26,45 @@ const renderUI = (data) => {
 const loadContent = async (key) => {
     console.log(`Cargando contenido para: ${key}`);
     const container = document.getElementById('dynamic-content');
+    clearInterval(typingInterval); // Si el usuario hace clic rápido, cancelamos la animación anterior
     container.innerText = ""; // Limpiamos el contenido anterior
     // container.innerText = "<p>Cargando...</p>"; // Indicador de carga
     // container.innerHTML = `<pre style="font-family: inherit;white-space: pre-wrap;">Cargando...<span class="typing-cursor"></span></pre>`; // Indicador de carga
 
     typeWriterEffect(container, "Cargando...");
 
-    if (key === 'cv') {
-        loadCV();
-        return;
+    if (key === 'cv') { loadCV(container); return; }
+
+    const rawContent = data_json.btn_content[key];
+    let textToType = "";
+
+    if (Array.isArray(rawContent)) {
+        rawContent.forEach(item => {
+            if (typeof item === 'object') {
+                textToType += `> ${item.title || item.name}\n`;
+                textToType += `  ${item.company || ''} ${item.time || ''}\n`;
+                textToType += `  ${item.description || item.desc}\n\n`;
+            } else {
+                textToType += `• ${item}\n`;
+            }
+        });
     }
+
+    clearInterval(typingInterval); // Si el usuario hace clic rápido, cancelamos la animación anterior
+    container.innerText = ""; // Limpiamos el contenido anterior
+
+    typeWriterEffect(container, textToType);
+
 }
 
-const loadCV = async () => {
+const loadCV = async (container) => {
     const langSelector = document.getElementById('lang-selector');
     const lang = langSelector.value;
     console.log(`Cargando CV en idioma: ${lang}`);
+    clearInterval(typingInterval); // Si el usuario hace clic rápido, cancelamos la animación anterior
+    container.innerText = ""; // Limpiamos el contenido anterior
+
+    typeWriterEffect(container, "404 NOT FOUND\n\nLo siento, el CV no está disponible en este momento. Por favor, contáctame para más información.");
     // langSelector.setAttribute("value", lang);
 }
 
@@ -49,7 +72,7 @@ const typeWriterEffect = (container, text) => {
     let index = 0;
 
     // 1. Creamos un único contenedor para el texto y el cursor
-    const wrapper = document.createElement('pre');
+    const wrapper = document.createElement('p');
     wrapper.className = "typing-wrapper";
     
     // 2. Creamos el span donde se escribirá el texto
